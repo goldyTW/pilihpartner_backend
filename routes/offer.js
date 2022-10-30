a9fc99bbdd83a947dee9c267fe1a62702f886ca9
goldy <goldywijaya@gmail.com>
2022-10-30 15:13:19 +0700

    add API

--- routes/offer.js
+++ routes/offer.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-import { createOffer, getOffer, updateOffer } from "../controllers/offer.js";
+import { createOffer, deleteOffer, getOffer, updateOffer, getSingleOffer } from "../controllers/offer.js";
 // import auth from "../middleware/auth.js";
 
 router.post("/create", createOffer);
 router.get("/:id", getOffer);
+router.get("/single/:id/:idteam", getSingleOffer);
 router.patch("/:id", updateOffer);
+router.delete("/:id/:idteam", deleteOffer)
 export default router;
